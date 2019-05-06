6b08c8623ec65cb8013c833bb1b4e62ca689700b
Zooheck <davidscottflack@gmail.com>
2019-05-06 16:43:52 -0500

    Solve MVP

--- queue/queue.py
+++ queue/queue.py
@@ -31,12 +31,32 @@ class Queue:
             self.tail = new_person
         # If the queue has more than one...
         else:
-          # the
             self.tail.set_next(new_person)
             self.tail = new_person
 
     def dequeue(self):
-        pass
+        # there are no people in the queue
+        if not self.head and not self.tail:
+            print('The list is currently empty.')
+            return None
+        # if there is only one item in the list
+        elif self.head == self.tail:
+            person = self.head
+            self.head = None
+            self.tail = None
+            print(f'{person.get_name()} has left the queue. The queue is now empty.')
+            return person.get_name()
+
+        else:
+            person = self.head
+            self.head = person.get_next()
+            return person.get_name()
 
     def len(self):
         pass
+        count = 0
+        current = self.head
+        while current is not None:
+            count += 1
+            current = current.get_next()
+        return int(count)
