55f1151aefd845bb6a404a86af4dd28ebfc2ced5
Zooheck <davidscottflack@gmail.com>
2019-05-08 15:23:34 -0500

    Solve all binary_search except callback method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,13 +5,45 @@ class BinarySearchTree:
         self.right = None
 
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     def contains(self, target):
-        pass
+        # if the base value is the target...
+        if self.value == target:
+            return True
+        # if the target is less than the base value, examine the left side
+        elif target < self.value:
+            # if there's nothing left of the base, the target does not exist
+            if not self.left:
+                return False
+            # return true if target is equal to left of base
+            elif target == self.left.value:
+                return True
+            # if target is unequal to left of base, run the function again with the left value as the new base
+            else:
+                self.left.contains(target)
+        elif target > self.value:
+            if not self.right:
+                return False
+            elif target == self.right.value:
+                return True
+            else:
+                self.right.contains(target)
 
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max()
 
     def for_each(self, cb):
         pass
