9f6a46725f91499d8d6dc1c575b8649373f4b6d6
Zooheck <davidscottflack@gmail.com>
2019-05-08 18:39:53 -0500

    Solve insert of max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,29 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        self.storage.append(value)
+        index = self.storage.index(value)
+        self._bubble_up(index)
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        return self.storage[0]
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        return len(self.storage)
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent
+            else:
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
